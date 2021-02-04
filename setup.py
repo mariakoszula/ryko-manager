1d047a9019cc395df170189930581d919609f125
Koszucka Maria <maria.koszucka@nokia.com>
2021-02-04 15:40:57 +0100

    Enable and use datapicker

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rykomanager',
-    version='3.9.11',
+    version='3.10',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
